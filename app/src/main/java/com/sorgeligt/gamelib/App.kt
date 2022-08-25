b0661d8f4e132de8e76cdb9cb70b2c203188d9fb
sorgeligt <ilch.a@yahoo.com>
2022-08-26 00:00:21 +0300

    Init

---
+++ app/src/main/java/com/sorgeligt/gamelib/App.kt
@@ -0,0 +1,6 @@
+package com.sorgeligt.gamelib
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/com/sorgeligt/gamelib/ui/mainscreen/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.sorgeligt.gamelib.ui.mainscreen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sorgeligt.gamelib.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(layoutInflater, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
